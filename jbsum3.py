83d5072fef425192add792492249bcfced2cb47d
jayanibhagya <91822301+jayanibhagya@users.noreply.github.com>
2022-10-17 01:08:07 +0530

    changes for jbsum3.py
    number vareable

--- jbsum3.py
+++ jbsum3.py
@@ -1,6 +1,6 @@
 #sum of two numbers 
 
-number1=int(input("Enter fest number"))
-number2=int(input("Enter second number"))
-total=number1-number2
+n1=int(input("Enter fest number"))
+n2=int(input("Enter second number"))
+total=n1-n2
 print("Total = ",total)
