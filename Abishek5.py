ba4729e3525e388ebcecd99342888134f36c03a7
Abishek-sl <115158246+Abishek-sl@users.noreply.github.com>
2022-10-17 10:52:56 -0700

    python

---
+++ Abishek5.py
@@ -0,0 +1,7 @@
+count = 1
+sum = 0
+while ( count <=5):
+x = f l o a t (input ( ” Enter i n t e g e r ”+str ( count )+” =>” ) )
+sum = sum + x
+count = count + 1
+pr int ( ”The sum of i n t e g e r s i s { 0 : 3 . 2 f }” . format (sum) )
